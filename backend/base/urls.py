d98a00c9a2aa90b02bc26b0c042e26233d9270e3
Tim La <email@example.com>
2021-04-15 12:14:40 -0400

    able to register user

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,7 +3,7 @@ from base import views
 
 urlpatterns = [
   path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-  path('', views.getRoutes, name='routes'),
+  path('users/register/', views.registerUser, name='register'),
   path('users/profile/', views.getUserProfile, name='users-profile'),
   path('users/', views.getUsers, name='users'),
   path('products/', views.getProducts, name='products'),
